5298dd3a160b096dec19bc635ab740dda23838c2
Konstantin Dryagin <gunster98@gmail.com>
2025-05-29 11:11:32 +0300

    fix: delete jsx

--- src/shared/ui/Button/ButtonUi.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import styles from './ButtonUi.module.css';
-const buttonUi = ({ children, variant = 'default', ...rest }) => {
-    return (_jsx("button", { className: `${styles[variant]} ${styles.base}`, ...rest, children: children }));
-};
-export default buttonUi;
